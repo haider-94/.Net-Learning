d99fdda749480f62db7ec95d9352cd90d2fe959d
haider <mail.haiderali94@gmail.com>
2026-07-07 21:17:35 +0500

    Linked db and created a table and added a row

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using QuickPortals.Models;
+
+namespace QuickPortals.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SetupCheck> SetupChecks { get; set; }
+    }
+}
